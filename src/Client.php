1ef4e73a91f7392321363bb416228daa1d378046
Craig McKie <craig@techk.at>
2024-03-29 16:26:21 +0000

    Updated upload method, added new uploadLargeFile method.

--- src/Client.php
+++ src/Client.php
@@ -70,25 +70,25 @@ class Client {
   public function __construct(string $keyId = '', string $applicationKey = '', array $options = [])
   {
     /*
-     * Store the two main parameters as a varible within the class.
+    ** Store the two main parameters as a varible within the class.
     */
     $this->keyId = $keyId;
     $this->applicationKey = $applicationKey;
 
     /*
-     * Set a length of time in seconds for how long an authorizationToken
-     * should stay in cache before being recycled.
+    ** Set a length of time in seconds for how long an authorizationToken
+    ** should stay in cache before being recycled.
     */
     $this->authTokenTimeout = isset($options['authTimeout']) ? $options['authTimeout'] : 43200;
 
     /*
-     * Backblaze B2 contains 2 versions of their API, though by default, v2 is the best selected option.
+    ** Backblaze B2 contains 2 versions of their API, though by default, v2 is the best selected option.
     */
     $this->version = (isset($option['version']) && $option['version'] == 1) ? self::B2_API_V1 : self::B2_API_V2;
 
     /*
-     * If the $keyId and/or $applicationKey is empty at runtime, throw an exception.
-     * The $keyId and $applicationKey is mandatory for this package to work.
+    ** If the $keyId and/or $applicationKey is empty at runtime, throw an exception.
+    ** The $keyId and $applicationKey is mandatory for this package to work.
     */
     if(empty($this->keyId) || empty($this->applicationKey))
     {
@@ -96,21 +96,21 @@ class Client {
     }
 
     /*
-     * If the authorizationToken needs to be recycled, you can force a re-authorization,
-     * which will clear the current token in cache and create a new one.
+    ** If the authorizationToken needs to be recycled, you can force a re-authorization,
+    ** which will clear the current token in cache and create a new one.
     */
     $forceRefresh = (isset($options['forceReauthorization']) && $options['forceReauthorization'] === true) ? true : false;
 
     /*
-     * Use the defined cache directory if one is set, or use the default.
+    ** Use the defined cache directory if one is set, or use the default.
     */
     $cacheDir = isset($options['cacheDir']) ? $options['cacheDir'] : __DIR__ . '/Cache';
 
     /*
-     * Create the cache container that opens access to the cache.
-     * Unless forceReauthorization is true, or the previous authorizationToken
-     * has expired and a new one needs to be generated, return the currently
-     * stored authorizationToken from cache.
+    ** Create the cache container that opens access to the cache.
+    ** Unless forceReauthorization is true, or the previous authorizationToken
+    ** has expired and a new one needs to be generated, return the currently
+    ** stored authorizationToken from cache.
     */
     $this->createCacheContainer($cacheDir);
     $this->authorizeAccount($forceRefresh);
@@ -140,8 +140,8 @@ class Client {
     $cacheKey = 'Backblaze-B2-SDK-Authorization-Token';
 
     /*
-     * If the $forceRefresh variable is true,
-     * force the cache to forget it before proceeding.
+    ** If the $forceRefresh variable is true,
+    ** force the cache to forget it before proceeding.
     */
     if($forceRefresh === true)
     {
@@ -149,9 +149,9 @@ class Client {
     }
 
     /*
-     * If the cache is missing the token or it has expired, have the method
-     * generate a new one and store it to the cache for re-use until its
-     * expiration.
+    ** If the cache is missing the token or it has expired, have the method
+    ** generate a new one and store it to the cache for re-use until its
+    ** expiration.
     */
     if(!$this->cache->has($cacheKey))
     {
@@ -160,7 +160,7 @@ class Client {
       ]);
 
       /*
-       * Store authorizationToken, apiUrl, downloadURL and recommendedPartSize to cache
+      ** Store authorizationToken, apiUrl, downloadURL and recommendedPartSize to cache
       */
       $this->cache->put($cacheKey, [
         'accountId'               => $response['accountId'],
@@ -173,20 +173,20 @@ class Client {
       ], $this->authTokenTimeout);
 
       /*
-       * This variable will be included in the response to let the user know if
-       * the cache contained an active token.
+      ** This variable will be included in the response to let the user know if
+      ** the cache contained an active token.
       */
       $this->loadedFromCache = false;
     }
 
     /*
-     * Grab the authorizationToken, apiUrl, downloadUrl and recommendedPartSize
-     * from cache and load into variables for use with each B2 Native API endpoint.
+    ** Grab the authorizationToken, apiUrl, downloadUrl and recommendedPartSize
+    ** from cache and load into variables for use with each B2 Native API endpoint.
     */
     $store = $this->cache->get($cacheKey);
 
     /*
-     * Store the necessary response to variables for use in the SDK.
+    ** Store the necessary response to variables for use in the SDK.
     */
     $this->accountId                = $store['accountId'];
     $this->absoluteMinimumPartSize  = $store['absoluteMinimumPartSize'];
@@ -213,7 +213,7 @@ class Client {
   private function createCacheContainer(string $cacheDir)
   {
     /*
-     * Check if cache directory exists. If not, create it.
+    ** Check if cache directory exists. If not, create it.
     */
     if(is_dir($cacheDir) === false)
     {
@@ -221,12 +221,12 @@ class Client {
     }
 
     /*
-     * Initialize the container
+    ** Initialize the container
     */
     $container = new Container();
 
     /*
-     * Set configuration for container
+    ** Set configuration for container
     */
     $container['config'] = [
       'cache.default' => 'file',
@@ -237,14 +237,14 @@ class Client {
     ];
 
     /*
-     * use Filesystem to store cache as files.
+    ** use Filesystem to store cache as files.
     */
     $container['files'] = new Filesystem;
 
     try
     {
       /*
-       * Set the cache container to globally-accessible variable.
+      ** Set the cache container to globally-accessible variable.
       */
       $cacheManager = new CacheManager($container);
       $this->cache = $cacheManager->store();
@@ -252,7 +252,7 @@ class Client {
     catch(Exception $e)
     {
       /*
-       * If an error occurs, throw an exception explaining why.
+      ** If an error occurs, throw an exception explaining why.
       */
       return throw new CacheException($e->getMessage());
     }
@@ -260,7 +260,7 @@ class Client {
 
   /*
   |--------------------------------------------------------------------------
-  | Quick Upload Function
+  | Quick Upload Function for small files
   |--------------------------------------------------------------------------
   |
   | A function to be able to quickly upload a file to BackBlaze B2.
@@ -273,44 +273,51 @@ class Client {
   |--------------------------------------------------------------------------
   |
   */
-  public function upload(string $filePath = '', string $fileName = '', string $bucketId = ''): File
+  public function upload(mixed $data, string $fileName = '', string $bucketId = ''): File
   {
-    /*
-     * Open the file for reading
-    */
-    $fileHandle = @fopen($filePath, 'r');
-
-    /*
-     * First check if file is readable.
-    */
-    if(!$fileHandle)
+    if(is_resource($data))
     {
-      return throw new FOpenException('Could not open file: ' . $filePath);
-    }
+      /*
+      ** Initialize hash_init
+      */
+      $initSha1 = hash_init('sha1');
+      hash_update_stream($initSha1, $data);
 
-    /*
-     * Get the original filesize and SHA1 hash of the file from filePath.
-    */
-    $fileSize = filesize($filePath);
-    $fileSha1 = sha1_file($filePath);
+      $fileSha1 = hash_final($initSha1);
+      $fileSize = fstat($data)['size'];
+
+      rewind($data);
+
+      $fileContent = stream_get_contents($data);
+    }
+    elseif(file_exists($data))
+    {
+      /*
+      ** Open the file for reading
+      ** and get filesize and file's SHA1 hash.
+      */
+      $fileSha1 = sha1_file($data);
+      $fileSize = filesize($data);
+      $fileContents = file_get_contents($data);
+    }
 
     if($fileSize <= 3000000000)
     {
       /*
-       * Start by getting an upload URL
+      ** Start by getting an upload URL
       */
       $uploadUrl = $this->getUploadUrl(['bucketId' => $bucketId]);
 
       /*
-       * Set options
+      ** Set options
       */
       $options = [
         'uploadUrl' => $uploadUrl['uploadUrl'],
-        'body'      => fread($fileHandle, $fileSize),
+        'body'      => $fileContent,
       ];
 
       /*
-       * Set headers
+      ** Set headers
       */
       $headers = [
         'Authorization'     => $uploadUrl['authorizationToken'],
@@ -321,118 +328,134 @@ class Client {
       ];
 
       /*
-       * Now let's upload the file to the uploadUrl
+      ** Now let's upload the file to the uploadUrl
       */
       $upload = $this->uploadFile($options, $headers);
 
       /*
-       * Close the file handle
+      ** At this point, file should be uploaded fully to the BackBlaze B2 Bucket.
+      ** Return a new File model of the uploaded file.
+      */
+      return new File($upload);
+    }
+  }
+
+  /*
+  |--------------------------------------------------------------------------
+  | Quick Upload Function for large files
+  |--------------------------------------------------------------------------
+  |
+  | A function to be able to quickly upload a large file to Backblaze B2.
+  |
+  |--------------------------------------------------------------------------
+  | @param  (string) $filePath | Default: (empty)
+  | @param  (string) $fileName | Default: (empty)
+  | @param  (string) $bucketId | Default: (empty)
+  | @return TechKat\BackBlazeB2\File | Exception
+  |--------------------------------------------------------------------------
+  |
+  */
+  public function uploadLargeFile(string $filePath = '', string $fileName = '', string $bucketId = '')
+  {
+    try
+    {
+      /*
+      ** Initialize a large file upload by creating an empty object in bucket.
+      */
+      $startLargeFile = $this->startLargeFile([
+        'bucketId' => $bucketId,
+        'fileName' => $fileName,
+      ]);
+
+      /*
+      ** Keep track of partNumbers, an array of all part SHA1s, and the fileId from startLargeFile.
+      */
+      $partNumber = 1;
+      $partSha1Array = [];
+      $fileId = $startLargeFile['fileId'];
+
+      /*
+     *** Open large file
+      */
+      $fileHandle = @fopen($filePath, 'r');
+
+      /*
+      ** Loop through chunks of the large file
+      */
+      while(!feof($fileHandle))
+      {
+        /*
+        ** Extract a chunk of data from large file,
+        ** and get a part upload URL for it.
+        */
+        $chunkData = fread($fileHandle, 10485760);
+        $partUploadUrl = $this->getUploadPartUrl(['fileId' => $fileId], true);
+
+        /*
+        ** Keep a note of the chunk's SHA1 hash, and add the SHA1 to the array.
+        */
+        $partSha1 = sha1($chunkData);
+        $partSha1Array[] = $partSha1;
+
+        /*
+        ** Set options
+        */
+        $options = [
+          'uploadPartUrl' => $partUploadUrl['uploadUrl'],
+          'body'          => $chunkData,
+        ];
+
+        /*
+        ** Set headers
+        */
+        $headers = [
+          'Authorization'     => $partUploadUrl['authorizationToken'],
+          'Content-Length'    => strlen($chunkData),
+          'X-Bz-Part-Number'  => $partNumber,
+          'X-Bz-Content-Sha1' => $partSha1,
+        ];
+
+        /*
+        ** Upload this chunk of data to BackBlaze under the same fileId.
+        */
+        $uploadPart = $this->uploadPart($options, $headers);
+
+        /*
+        ** Increment the part number variable.
+        */
+        $partNumber++;
+      }
+
+      /*
+      ** Close the file handle
       */
       fclose($fileHandle);
 
       /*
-       * At this point, file should be uploaded fully to the BackBlaze B2 Bucket.
-       * Return a new File model of the uploaded file.
+      ** Finish large file upload
       */
-      return new File($upload);
+      $finishLargeFile = $this->finishLargeFile([
+        'fileId' => $fileId,
+        'partSha1Array' => $partSha1Array,
+      ]);
+
+      /*
+      ** Return the response of finishLargeFile
+      */
+      return new File($finishLargeFile);
     }
-    else
+    catch(GuzzleHttpClientException | ValidationException $e)
     {
-      try
-      {
-        /*
-         * Initialize a large file upload by creating an empty object in bucket.
-        */
-        $startLargeFile = $this->startLargeFile([
-          'bucketId' => $bucketId,
-          'fileName' => $fileName,
-        ]);
+      /*
+      ** An exception was observed somewhere, but BackBlaze will keep the large file object.
+      ** To keep things tidy in a bucket, we will cancel the large file.
+      */
+      $this->cancelLargeFile(['fileId' => $fileId]);
 
-        /*
-         * Keep track of partNumbers, an array of all part SHA1s, and the fileId from startLargeFile.
-        */
-        $partNumber = 1;
-        $partSha1Array = [];
-        $fileId = $startLargeFile['fileId'];
-
-        /*
-         * Loop through chunks of the large file
-        */
-        while(!feof($fileHandle))
-        {
-          /*
-           * Extract a chunk of data from large file,
-           * and get a part upload URL for it.
-          */
-          $chunkData = fread($fileHandle, 10485760);
-          $partUploadUrl = $this->getUploadPartUrl(['fileId' => $fileId], true);
-
-          /*
-           * Keep a note of the chunk's SHA1 hash, and add the SHA1 to the array.
-          */
-          $partSha1 = sha1($chunkData);
-          $partSha1Array[] = $partSha1;
-
-          /*
-           * Set options
-          */
-          $options = [
-            'uploadPartUrl' => $partUploadUrl['uploadUrl'],
-            'body'          => $chunkData,
-          ];
-
-          /*
-           * Set headers
-          */
-          $headers = [
-            'Authorization'     => $partUploadUrl['authorizationToken'],
-            'Content-Length'    => strlen($chunkData),
-            'X-Bz-Part-Number'  => $partNumber,
-            'X-Bz-Content-Sha1' => $partSha1,
-          ];
-
-          /*
-           * Upload this chunk of data to BackBlaze under the same fileId.
-          */
-          $uploadPart = $this->uploadPart($options, $headers);
-
-          /*
-           * Increment the part number variable.
-          */
-          $partNumber++;
-        }
-
-        /*
-         * Close the file handle
-        */
-        fclose($fileHandle);
-
-        /*
-         * Finish large file upload
-        */
-        $finishLargeFile = $this->finishLargeFile([
-          'fileId' => $fileId,
-          'partSha1Array' => $partSha1Array,
-        ]);
-
-        /*
-         * Return the response of finishLargeFile
-        */
-        return new File($finishLargeFile);
-      }
-      catch(GuzzleHttpClientException | ValidationException $e)
-      {
-        /*
-         * An exception was observed somewhere, but BackBlaze will keep the large file object.
-         * To keep things tidy in a bucket, we will cancel the large file.
-        */
-        $this->cancelLargeFile(['fileId' => $fileId]);
-
-        /*
-         * Return the exception that occurred.
-        */
-        return throw new Exception($e->getMessage());
-      }
+      /*
+      ** Return the exception that occurred.
+      */
+      return throw new Exception($e->getMessage());
     }
   }
 
@@ -456,7 +479,7 @@ class Client {
   protected function request(string $method = 'GET', string $uri = null, array $options = [], array $headers = [], bool $asJson = true): mixed
   {
     /*
-     * If Authorization header is not detected in the $headers parameter, set it using the existing authorizationToken
+    ** If Authorization header is not detected in the $headers parameter, set it using the existing authorizationToken
     */
     if(!is_null($this->authorizationToken) && !isset($headers['Authorization']))
     {
@@ -464,24 +487,24 @@ class Client {
     }
 
     /*
-     * Combine both headers and options into one array.
+    ** Combine both headers and options into one array.
     */
     $options = array_replace_recursive([
       'headers' => $headers,
     ], $options);
 
     /*
-     * Set the base URI for the HttpClient handling the request.
-     * If the apiUrl variable is set, use it - else use the B2_API_BASE_URL as default.
-     *
-     * After this, pass the baseUri variable to the HttpClient and the preferred API version.
+    ** Set the base URI for the HttpClient handling the request.
+    ** If the apiUrl variable is set, use it - else use the B2_API_BASE_URL as default.
+    **
+    ** After this, pass the baseUri variable to the HttpClient and the preferred API version.
     */
     $baseUri = isset($this->apiUrl) ? $this->apiUrl : self::B2_API_BASE_URL;
     $guzzleClient = new HttpClient(['base_uri' => $baseUri . $this->version]);
 
     /*
-     * Retrieve the response and return as a json_encoded body or not,
-     * depending on if $asJson is true or false.
+    ** Retrieve the response and return as a json_encoded body or not,
+    ** depending on if $asJson is true or false.
     */
     $response = $guzzleClient->request($method, $uri, $options);
     return ($asJson) ? json_decode($response->getBody(), true) : $response->getBody();
@@ -503,8 +526,8 @@ class Client {
   public function __call($method, $parameters)
   {
     /*
-     * List of all available traits that are accessible.
-     * Over time, as BackBlaze add more API endpoints, the list will be extended.
+    ** List of all available traits that are accessible.
+    ** Over time, as BackBlaze add more API endpoints, the list will be extended.
     */
     $traits = [
       'cancelLargeFile', 'copyFile', 'copyPart', 'createBucket',
@@ -517,13 +540,13 @@ class Client {
     ];
 
     /*
-     * Convert method into StudlyCase format.
-     * i.e uploadFile references TechKat\BackBlazeB2\NativeAPI\UploadFile
+    ** Convert method into StudlyCase format.
+    ** i.e uploadFile references TechKat\BackBlazeB2\NativeAPI\UploadFile
     */
     $callback = ucwords($method);
 
     /*
-     * If the trait exists, call it and pass the parameters through.
+    ** If the trait exists, call it and pass the parameters through.
     */
     if(in_array($method, $traits) && is_callable($method))
     {
@@ -531,7 +554,7 @@ class Client {
     }
 
     /*
-     * By default, an exception will be thrown if the trait does not exist.
+    ** By default, an exception will be thrown if the trait does not exist.
     */
     throw new BadNativeApiEndpointException(sprintf('Method %s::%s does not exist.', static::class, $method));
   }
